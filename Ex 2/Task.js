2f907c42769e90fe3a815d2ea9c293ff9f5c3161
JENCYSPARK <jencyshagayasalini22@gmail.com>
2026-05-12 16:05:42 +0530

    Delete Ex 2 directory

--- Ex 2/Task.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const taskSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
-    text: { type: String, required: true }
-});
-
-module.exports = mongoose.model('Task', taskSchema);
